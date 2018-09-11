c0e3b2b793135e26d16a9122e075388d20294d62
shayshae5482 <shayshae5482@gmail.com>
2018-09-10 19:47:32 -0500

    updated icons

---
+++ friends.js
@@ -0,0 +1,39 @@
+//array that holds all the 'friends' that have taken the survey
+//initialize starting with 'dummy' data
+var friendList = [
+    {
+      name: 'Ophelia Matthews',
+      profilePic: 'http://lh3.googleusercontent.com/sxRyzKgRjdnvxuO-B2WUILy2lzF8wxMuvPISRMv7k7U6EyOcUG7NW0-1mLeNyc5NajIWKn4i-r8vkEpTnO8toQ=s0',
+      scores: [
+        "4",
+        "5",
+        "5",
+        "2",
+        "4",
+        "5",
+        "4",
+        "4",
+        "3",
+        "4"
+      ]
+    },
+
+    {
+        name: 'Famous Amos',
+        profilePic: 'https://placeimg.com/640/480/people',
+        scores: [
+          "1",
+          "2",
+          "3",
+          "2",
+          "4",
+          "4",
+          "5",
+          "2",
+          "3",
+          "5"
+        ]
+      }
+  ]
+  
+  module.exports = friendList;
--- htmlRoutes.js
+++ htmlRoutes.js
@@ -1,46 +1,18 @@
 // Dependencies
 // ===========================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
-var PORT = 8080;
+var path = require('path');
 
-//Get the Express app to breakdown the json
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+//ROUTING
 
-// Data
-// ===========================================================
-// var yoda = {
-//     name: "Yoda",
-//     role: "Jedi Master",
-//     age: 900,
-//     forcePoints: 2000
-//   };
-  
-//   var darthmaul = {
-//     name: "Darth Maul",
-//     role: "Sith Lord",
-//     age: 200,
-//     forcePoints: 1200
-//   };
-///
-//==============================================================
-
-
-// Routes
-// ===========================================================
-app.get("/home", function(req, res) {
-    res.send("Welcome to Friend Finder!");
+module.exports = function(app){
+  //default GET route that leads to home.html - displays home page
+  app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname + '/survey.html'));
   });
-  
-  app.get("/survey", function(req, res) {
-    res.json(data);
+
+  //a USE route to home page
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname + '/home.html'));
   });
-  
-  // Listener
-// ===========================================================
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+};
   
